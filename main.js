183379b62a425387790971e8610c8dc248443d80
WaiYanLinn <wl1744207@gmail.com>
2026-04-23 16:57:25 +0900

    my first commit

---
+++ main.js
@@ -0,0 +1,8 @@
+// const name = prompt("Your Name?");
+// alert("Hello!" + name );
+
+const target = document.getElementById("target");
+target.textContent = "Hello!";
+target.style.color ="red";
+target.style.fontSize = "10px";
+target.style.backgroundColor = "yellow"
