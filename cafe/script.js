7b5031c8677f518dbeade73c0b81de33c2e0b16f
WaiYanLinn <wl1744207@gmail.com>
2026-05-28 15:56:13 +0900

    using if and else

--- cafe/script.js
+++ cafe/script.js
@@ -1,8 +1,22 @@
 // form
 function reserve() {
+    // Get the values from the form inputs
     const name = document.getElementById('guestName').value;
     const count = document.getElementById('guestCount').value;
 
+    // Make the reservation (this is just a simulation)
     const result = document.getElementById('reserveResult');
-    result.textContent = `Reservation for ${name} with ${count} guests has been made! Thank you for your reservation`;
+
+    // If the name or number of people is empty, display a warning message and exit.
+    if (name === '' || count === '') {
+        result.textContent = 'Please enter your name.';
+        return;
+    } else {
+        result.textContent = `Reservation for ${name} with ${count} guests has been made! Thank you for your reservation`;
+    }
+}
+
+// recommend
+function pickRecommend() {
+    
 }
